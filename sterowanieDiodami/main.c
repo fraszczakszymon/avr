48b2a9bf83f9476725eadd3ff4eb4293ed27425f
fraszczakszymon <fraszczak.szymon@gmail.com>
2012-11-19 18:04:19 +0100

    Dodany trzeci przycisk D2, sterujacy aktywna dioda

--- sterowanieDiodami/main.c
+++ sterowanieDiodami/main.c
@@ -4,13 +4,18 @@
 int main(void)
 {
   int status = 0;
+  int activeLed = 0;
   DDRB = 0x03;
   PORTB = 0x01;
   DDRD = 0x00;
-  PORTD = 0x03;
+  PORTD = 0x07;
 
   while (1) {
-    if (PIND & 0x02) {
+    if (!(PIND & 0x02)) {
+      PORTB ^= 0x01;
+      PORTB ^= 0x02;
+      _delay_ms(150);
+    } else {
       if (PIND & 0x01) {
         status = 0;
         _delay_ms(50);
@@ -22,10 +27,19 @@ int main(void)
         }
         _delay_ms(50);
       }
-    } else {
-      PORTB ^= 0x01;
-      PORTB ^= 0x02;
-      _delay_ms(300);
+    }
+
+    if (!(PIND & 0x04) && (PIND & 0x02)) {
+      if (PORTB & 0xFF) {
+        activeLed = PORTB;
+      }
+      if (activeLed == 1) {
+        PORTB ^= 0x01;
+        _delay_ms(200);
+      } else if (activeLed == 2) {
+        PORTB ^= 0x02;
+        _delay_ms(200);
+      }
     }
   }
 }
